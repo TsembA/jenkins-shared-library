6a457ba3ce133facddfe82d61d4803bc13e31b25
Arno Tsembenhoi <tsembenhoi@gmail.com>
2024-09-11 16:52:18 -0700

    Rewrite credentials

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,7 +4,7 @@
 def call () {
     def buildImage() {
     echo 'build and push image...'
-    withCredentials([usernamePassword(credentialsId: 'my-pipeline', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t tsemb/demo-app:jma-6.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push tsemb/demo-app:jma-6.0'
