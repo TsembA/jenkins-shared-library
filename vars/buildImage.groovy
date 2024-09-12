9372e7bb823a25304ebbded6d796b9e2b5544c8f
Tsemb <82046636+TsembA@users.noreply.github.com>
2024-09-11 17:49:21 -0700

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,10 @@
-#!/usr/bin/env groovy
+#!/user/bin/env groovy
 
 def call() {
-    def buildImage() {
-        echo 'build and push image...'
-        withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            sh 'docker build -t tsemb/demo-app:jma-6.0 .'
-            sh 'echo $PASS | docker login -u $USER --password-stdin'
-            sh 'docker push tsemb/demo-app:jma-6.0'
-        }
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t tsemb/demo-app:jma-6.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push tsemb/demo-app:jma-6.0'
     }
 }
