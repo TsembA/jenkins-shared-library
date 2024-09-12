0082f2ccebacc74062607311b8fcb717eb6cb615
Arno Tsembenhoi <tsembenhoi@gmail.com>
2024-09-11 17:27:25 -0700

    Adding the new version of the docker image

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -5,9 +5,9 @@ def call () {
     def buildImage() {
     echo 'build and push image...'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t tsemb/demo-app:jma-333 .'
+        sh 'docker build -t tsemb/demo-app:jma-6.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push tsemb/demo-app:jma-222'
+        sh 'docker push tsemb/demo-app:jma-6.0'
     }
 
 }
